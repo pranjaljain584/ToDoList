28ef616263f434a05186bc321234464fa0fd1997
pranjaljain584 <62601082+pranjaljain584@users.noreply.github.com>
2020-09-05 23:25:15 +0530

    add assets and views and some changes to home ejs and home css

--- index.js
+++ index.js
@@ -2,16 +2,15 @@ const express = require('express') ;
 const app = express() ;
 const port =8000 ;
 
-// use static files
-app.use(express.static('./assets')) ;
-
 // use express router
 app.use('/' , require('./routes/index')) ;
+
+// use static files
+app.use(express.static('./assets')) ;
 
 // set up view engine
 app.set('view engine' , 'ejs') ;
 app.set('views' , './views') ;
-
 
 // made app listen
 app.listen(port , function(err){
