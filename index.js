304a74fec12e37004c197a91d52097bdc1ae1b9d
pranjaljain584 <62601082+pranjaljain584@users.noreply.github.com>
2020-09-05 14:02:46 +0530

    set up first route and controller

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express') ;
 const app = express() ;
 const port =8000 ;
 
+// use express router
+app.use('/' , require('./routes/index')) ;
 
 
 app.listen(port , function(err){
