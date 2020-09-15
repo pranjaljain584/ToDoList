b919cd687f8eaaee6b0d12cec92830d4ce8b76ad
pranjaljain584 <62601082+pranjaljain584@users.noreply.github.com>
2020-09-15 16:58:39 +0530

    task detail styling done

--- assets/js/home.js
+++ assets/js/home.js
@@ -9,7 +9,40 @@ $(".det-btn").click(function(){
     let description = $(this).attr('data-description') ; 
     let id = $(this).attr('data-id') ;
     console.log(taskName, taskDate , category , priority , description , id) ;
-    // idDiv.html( taskName );
+    $("#tnm").html( taskName );
+    $("#tdt").html( taskDate );
+    $("#tcg").html( category );
+
+    if(category == "Work"){
+        $('.c-btn').css("background-color" , "rgb(143, 7, 143)") ;
+
+    }else if(category == "Personal"){
+        $('.c-btn').css("background-color" , "#3D6AB8") ;
+
+    }else if(category == "Study"){
+        $('.c-btn').css("background-color" , "#87431d") ;
+        
+    }else{
+        $('.c-btn').css("background-color" , "gray") ;  
+    }
+
+    $("#tpr").html( priority );
+
+    if(priority == "imp"){
+        $('.p-btn').css("background-color" , "#E74C3C") ;
+
+    }else if(priority == "urgent"){
+        $('.p-btn').css("background-color" , "#F1C40F") ;
+
+    }else if(priority == "notUrgent"){
+        $('.p-btn').css("background-color" , "#3498DB") ;
+        
+    }else{
+        $('.p-btn').css("background-color" , "#2ECC71") ;  
+    }
+
+    $("#tde").html(description);
+
     $("#delete-task-btn").attr('href' , `/delete-contact/?id=${id}`) ;
 
 }) ; 
